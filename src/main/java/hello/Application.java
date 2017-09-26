145f70b5f9359f2a5d5644e863454f03208c99fd
Ankita Gupta <ankita1.gupta@paytm.com>
2017-09-26 12:49:41 +0530

    added jvm.sh

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    @Autowired
+    Sender sender;
+
     @RequestMapping("/")
     public String home() {
+        sender.send("helloworld.t","hello");
         return "Hello Docker World";
+
     }
 
     public static void main(String[] args) {
--- src/main/java/hello/Sender.java
+++ src/main/java/hello/Sender.java
@@ -3,8 +3,12 @@ package hello;
 import javax.annotation.ManagedBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @ManagedBean
 public class Sender {
@@ -16,6 +20,20 @@ public class Sender {
 
   public void send(String topic, String payload) {
     LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
-    kafkaTemplate.send(topic, payload);
+
+    ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic,payload);
+    future.addCallback(new ListenableFutureCallback<SendResult<String,String>>() {
+
+      @Override
+      public void onSuccess(SendResult<String, String> result) {
+        LOGGER.info("successfully sent msg to kafka");
+      }
+
+      @Override
+      public void onFailure(Throwable ex) {
+        LOGGER.info("failed to send msg to kafka");
+      }
+
+    });
   }
 }
